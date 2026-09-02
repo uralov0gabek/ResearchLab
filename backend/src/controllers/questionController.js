cea2a691807d9dbcb333e277aab54b1a8ca09110
Og'abek <uralovogabek321@gmail.com>
2026-09-02 17:48:24 +0500

    fix: remove in-memory question caching to prevent state mismatch with database

--- backend/src/controllers/questionController.js
+++ backend/src/controllers/questionController.js
@@ -1,14 +1,8 @@
 const questionService = require('../services/questionService');
 
-let questionsCache = null;
-
 const getQuestions = async (req, res, next) => {
   try {
-    if (questionsCache) {
-      return res.json(questionsCache);
-    }
     const data = await questionService.fetchQuestions();
-    questionsCache = data;
     res.json(data);
   } catch (error) {
     next(error);
@@ -19,7 +13,6 @@ const upsertQuestions = async (req, res, next) => {
   try {
     const { questionsToUpsert, idsToDelete } = req.body;
     await questionService.saveQuestions(questionsToUpsert, idsToDelete);
-    questionsCache = null; // Clear cache on update
     res.json({ success: true });
   } catch (error) {
     next(error);
